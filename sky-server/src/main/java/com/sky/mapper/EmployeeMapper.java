884124d69c1675b097369fda8f058d42fa1f935b
彭文成 <2401554207@qq.com>
2025-05-21 16:45:13 +0800

    编辑员工信息代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -33,17 +33,4 @@ public interface EmployeeMapper {
      * @return
      */
     Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
-
-    /**
-     * 根据主键动态修改属性
-     */
-    void update(Employee employee);
-
-    /**
-     * 根据id查询员工信息
-     * @param id
-     * @return
-     */
-    @Select("select * from employee where id = #{id}")
-    Employee getById(Long id);
 }
